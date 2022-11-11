2cefd7628a3dea9f85493532e895d215f8226ff3
José Del Valle <dell2793@gmail.com>
2022-11-11 11:53:15 -0600

    Formats (prettier) operations.ts

--- src/utils/operations.ts
+++ src/utils/operations.ts
@@ -11,39 +11,39 @@ export function multiply(a: number, b: number): number {
 }
 
 export function divide(a: number, b: number): number {
-  if(!b) {
+  if (!b) {
     return 0;
   }
 
   return a / b;
 }
 
-
-const OPERATOR_FUNCTIONS: Record<string, (a: number, b:number) => number> = {
+const OPERATOR_FUNCTIONS: Record<string, (a: number, b: number) => number> = {
   ["+"]: add,
   ["-"]: subtract,
   ["*"]: multiply,
-  ["/"]: divide
-}
+  ["/"]: divide,
+};
 
 export function calculate(input: string, operands: number[]): number[] {
-  const inputElements = input.split(" ").filter(e => e);
+  const inputElements = input.split(" ").filter((e) => e);
 
-  inputElements.forEach(function(element) {
+  inputElements.forEach(function (element) {
     const isNumber = !isNaN(element as any);
-    const isOperator = !isNumber && Object.keys(OPERATOR_FUNCTIONS).includes(element);
+    const isOperator =
+      !isNumber && Object.keys(OPERATOR_FUNCTIONS).includes(element);
 
-    if(isNumber) {
-      operands = [...operands, Number(element)]
-    } else if(isOperator) {
+    if (isNumber) {
+      operands = [...operands, Number(element)];
+    } else if (isOperator) {
       const operator = element;
-      const operation = OPERATOR_FUNCTIONS[operator]
+      const operation = OPERATOR_FUNCTIONS[operator];
       const b = operands.pop() || 0;
       const a = operands.pop() || 0;
       const result = operation(a, b);
       operands = result ? [...operands, result] : operands;
     }
-  })
+  });
 
-  return operands.length ? operands : [0]
+  return operands.length ? operands : [0];
 }
