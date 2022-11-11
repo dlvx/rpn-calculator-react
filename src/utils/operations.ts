efd94c4fc502acdfa2f81a450e5d57423f7b60c2
José Del Valle <dell2793@gmail.com>
2022-11-11 11:14:21 -0600

    Adds operations.ts file

---
+++ src/utils/operations.ts
@@ -0,0 +1,49 @@
+export function add(a: number, b: number): number {
+  return a + b;
+}
+
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
+
+export function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+export function divide(a: number, b: number): number {
+  if(!b) {
+    return 0;
+  }
+
+  return a / b;
+}
+
+
+const OPERATOR_FUNCTIONS: Record<string, (a: number, b:number) => number> = {
+  ["+"]: add,
+  ["-"]: subtract,
+  ["*"]: multiply,
+  ["/"]: divide
+}
+
+export function calculate(input: string, operands: number[]): number[] {
+  const inputElements = input.split(" ").filter(e => e);
+
+  inputElements.forEach(function(element) {
+    const isNumber = !isNaN(element as any);
+    const isOperator = !isNumber && Object.keys(OPERATOR_FUNCTIONS).includes(element);
+
+    if(isNumber) {
+      operands = [...operands, Number(element)]
+    } else if(isOperator) {
+      const operator = element;
+      const operation = OPERATOR_FUNCTIONS[operator]
+      const b = operands.pop() || 0;
+      const a = operands.pop() || 0;
+      const result = operation(a, b);
+      operands = result ? [...operands, result] : operands;
+    }
+  })
+
+  return operands.length ? operands : [0]
+}
