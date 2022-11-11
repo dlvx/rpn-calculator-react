5f441d4590476725b826ac3fb638d1dd69b971c8
José Del Valle <dell2793@gmail.com>
2022-11-11 11:38:34 -0600

    Implements a CLI using react-terminal-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Terminal, { ColorMode, TerminalOutput } from "react-terminal-ui";
+import "./App.css";
+import { calculate } from "./utils/operations";
 
 function App() {
+  const [operands, setOperands] = useState<number[]>([]);
+
+  const inputHandler = (input: string) => {
+    const pass = input.match(/^[0-9qc+\-*\/\s]*$/);
+
+    if (pass) {
+      if (input === "c" || input === "q") {
+        setOperands([]);
+      } else {
+        const res = calculate(input, operands);
+        setOperands(res);
+      }
+    } else {
+      alert(
+        "Only numeric values and the operators: + - * / are allowed. Enter 'c' to clear the stored values and 'q' to exit."
+      );
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div style={{ textAlign: "left" }}>
+          <p>Instructions:</p>
+          <p>
+            - Use reverse polish notation to input your operands and operators,
+            you can input an entire set like this, using a space as delimiter: 5
+            5 5 8 + + -
+          </p>
+          <p>
+            - You can also input operand by operand, hitting enter in between.
+            The operands will be added to a stack for later use by an operator.
+            Calculations will execute once you enter an operator.
+          </p>
+          <p>
+            - In both scenarios, an operator needs two preceeding operands,
+            otherwise it will be disregarded.
+          </p>
+          <p>- The allowed operators are: + - / *</p>
+          <p>- An empty input will be interpreted as a 0.</p>
+          <p>- Type 'c' if you want to clear your stack.</p>
+          <p>
+            - <strong>In this version only:</strong> Typing 'q' will clear the
+            stack instead of quitting.
+          </p>
+          <p>Have fun!</p>
+        </div>
+        <Terminal
+          name="RPN Calculator React"
+          colorMode={ColorMode.Light}
+          onInput={inputHandler}
         >
-          Learn React
-        </a>
+          <TerminalOutput>Result:</TerminalOutput>
+          {operands[operands.length - 1]}
+        </Terminal>
       </header>
     </div>
   );
